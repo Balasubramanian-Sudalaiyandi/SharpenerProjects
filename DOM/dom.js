6d48e7cd63c1c7535eeaade0b822824c78265b9d
Balasubramanian-Sudalaiyandi <bala281197@gmail.com>
2023-03-10 23:56:31 +0530

    second

---
+++ DOM/dom.js
@@ -0,0 +1,12 @@
+//console.dir(document);
+//console.log(document.getElementById('header-title'));
+  //  var title = document.getElementById('header-title');
+    //console.log(title);
+  //  var header = document.getElementById('main-header');
+//    header.style.borderBottom = 'solid 3px #000';
+var item = document.getElementsByClassName('list-group-item');
+console.log(item);
+for (var i = 0; i<item.length;i++){
+    item[i].style.color = 'Green';
+    item[i].style.fontWeight = 'bold';
+}
