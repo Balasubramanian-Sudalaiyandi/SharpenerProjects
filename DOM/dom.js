09efbb5455e68a52e4204beb1cacd947540dfe1e
Balasubramanian-Sudalaiyandi <bala281197@gmail.com>
2023-03-11 14:54:25 +0530

    new branch

--- DOM/dom.js
+++ DOM/dom.js
@@ -4,12 +4,12 @@
     //console.log(title);
   //  var header = document.getElementById('main-header');
 //    header.style.borderBottom = 'solid 3px #000';
-// var item = document.getElementsByClassName('list-group-item');
-// console.log(item);
-// for (var i = 0; i<item.length;i++){
-//     item[i].style.color = 'Green';
-//     item[i].style.fontWeight = 'bold';
-// } 
+var item = document.getElementsByClassName('list-group-item');
+console.log(item);
+for (var i = 0; i<item.length;i++){
+    item[i].style.color = 'Green';
+    item[i].style.fontWeight = 'bold';
+} 
 var item = document.getElementsByTagName('li');
 console.log(item);
 for (var i = 0; i<item.length;i++){
